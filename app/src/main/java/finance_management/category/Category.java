0bf7b341df78d4c3a7bdbe63e0261183b8bbd288
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2024-05-29 22:05:26 +0200

    feat: category created

---
+++ app/src/main/java/finance_management/category/Category.java
@@ -0,0 +1,25 @@
+package finance_management.category;
+
+public enum Category {
+    GROCERY,
+    EAT_OUT,
+    SAVINGS,
+    SALARY,
+    CLOTHES,
+    GIFT,
+    TRAVEL,
+    HOUSE,
+    TECHNOLOGY,
+    CONDOM,
+    RENT,
+    LAUNDRY,
+    CAR,
+    DENTISTRY,
+    MEDICAL_CHECK,
+    WORK_FOOD,
+    CAR_WASHING,
+    BARBER_SHOP,
+    MEDICINE,
+    ENTERTAINMENT,
+    ENERGY
+}
