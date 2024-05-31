f10e91fac943acc43ecacefa292ffb7b21048cee
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2024-05-31 23:03:56 +0200

    feat: Income class completed

--- app/src/main/java/finance_management/model/Income.java
+++ app/src/main/java/finance_management/model/Income.java
@@ -2,6 +2,7 @@ package finance_management.model;
 
 import finance_management.category.Category;
 import org.checkerframework.checker.units.qual.C;
+import java.time.LocalDateTime;
 
 import java.util.Arrays;
 
@@ -12,12 +13,33 @@ public class Income {
     private Category category;
     private float amount;
     private String description;
+    private LocalDateTime date;
 
     private final Category[] categories = new Category[] {
             Category.SALARY,
             Category.GIFT,
             Category.SAVINGS
     };
+
+    public Income(String category, float amount, String description, LocalDateTime date) {
+        if (!(isCategoryValid(category, categories) || isAmountValid(amount) || isDescriptionValid(description))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.category = Category.valueOf(category);
+            this.amount = amount;
+            this.description = description;
+            this.date = date;
+        }
+    }
+
+    public boolean isAmountValid(float amount) {
+        float MINIMUM_THRESHOLD = 0.0F;
+        return amount > MINIMUM_THRESHOLD;
+    }
+
+    public boolean isDescriptionValid(String description) {
+        return !description.isEmpty();
+    }
 
     public Category getCategory() {
         return category;
@@ -43,22 +65,12 @@ public class Income {
         this.description = description;
     }
 
-    public Income(String category, float amount, String description) {
-        if (!(isCategoryValid(category, categories) || isAmountValid(amount) || isDescriptionValid(description))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.category = Category.valueOf(category);
-            this.amount = amount;
-            this.description = description;
-        }
+    public LocalDateTime getDate() {
+        return date;
     }
 
-    public boolean isAmountValid(float amount) {
-        float MINIMUM_THRESHOLD = 0.0F;
-        return amount > MINIMUM_THRESHOLD;
+    public void setDate(LocalDateTime date) {
+        this.date = date;
     }
 
-    public boolean isDescriptionValid(String description) {
-        return !description.isEmpty();
-    }
 }
