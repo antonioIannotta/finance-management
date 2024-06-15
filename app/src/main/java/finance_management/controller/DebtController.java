6ee619d17c620c53ed525b07afd745e59d41a465
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2024-06-15 18:33:55 +0200

    feat: debt almost completed

--- app/src/main/java/finance_management/controller/DebtController.java
+++ app/src/main/java/finance_management/controller/DebtController.java
@@ -11,7 +11,9 @@ import finance_management.mongo.Mongo;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import javax.print.Doc;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DebtController {
@@ -27,6 +29,7 @@ public class DebtController {
                     .append("original_amount", Float.toString(debt.getOriginalAmount()))
                     .append("remaining_amount", Float.toString(debt.getRemainingAmount()))
                     .append("remaining_instalments", Integer.toString(debt.getRemainingInstalments()))
+                    .append("instalments_amount", Float.toString(debt.getInstalmentAmount()))
                     .append("description", debt.getDescription().toLowerCase())
                     .append("debt_category", debt.getDebtCategory().toString().toLowerCase())
                     .append("payments_date", debt.getPaymentsDate())
@@ -43,8 +46,49 @@ public class DebtController {
         return returnDebtFromQuery(searchQuery);
     }
 
-    public static void payInstalments(int numberInstalments, DebtCategory debtCategory) {
-        //TODO
+    public static void payInstalments(int numberInstalments, DebtCategory debtCategory, LocalDateTime date) {
+        var debt = retrieveDebtByCategory(debtCategory);
+        float paymentAmount = debt.getInstalmentAmount() * numberInstalments;
+        debt.setRemainingAmount(debt.getRemainingAmount() - paymentAmount);
+        debt.setRemainingInstalments(debt.getRemainingInstalments() - numberInstalments);
+        debt.setInstalmentAmount(debt.getRemainingAmount() / debt.getRemainingInstalments());
+        var updatedMap = debt.getPaymentsDate();
+        updatedMap.put(date, paymentAmount);
+        debt.setPaymentsDate(updatedMap);
+
+        updateDebt(debtCategory, debt);
+    }
+
+    public static void updateDebt(DebtCategory debtCategory, Debt debt) {
+        var updatedDebtDocument = getDocumentDebt(debt);
+        var searchQuery = new Document().append("debt_category", debtCategory.toString().toLowerCase());
+
+        Document updateObject = new Document();
+        updateObject.put("$set", updatedDebtDocument);
+
+        debtCollection.updateOne(searchQuery, updateObject);
+    }
+
+    private static Document getDocumentDebt(Debt debt) {
+        var searchQuery = new Document();
+        var updatedDocument = new Document();
+        searchQuery.put("debt_category", debt.getDebtCategory().toString().toLowerCase());
+        FindIterable<Document> cursor = debtCollection.find(searchQuery);
+        try (final MongoCursor<Document> cursorIterator = cursor.cursor()) {
+            if (cursorIterator.hasNext()) {
+                var debtDocument = cursorIterator.next();
+                updatedDocument.put("_id", debtDocument.get("_id"));
+            }
+        }
+        updatedDocument.put("debt_category", debt.getDebtCategory().toString().toLowerCase());
+        updatedDocument.put("description", debt.getDescription().toLowerCase());
+        updatedDocument.put("original_amount", Float.toString(debt.getOriginalAmount()));
+        updatedDocument.put("remaining_amount", Float.toString(debt.getRemainingAmount()));
+        updatedDocument.put("instalments_amount", Float.toString(debt.getInstalmentAmount()));
+        updatedDocument.put("remaining_instalments", Integer.toString(debt.getRemainingInstalments()));
+        updatedDocument.put("payments_date", getDocumentFromMap(debt.getPaymentsDate()));
+
+        return updatedDocument;
     }
 
     private static Debt returnDebtFromQuery(Document query) {
@@ -66,7 +110,19 @@ public class DebtController {
     }
 
     private static Map<LocalDateTime, Float> returnMapFromDocument(Document paymentsSubDocument) {
-        //TODO
+        var paymentsMap = new HashMap<LocalDateTime, Float>();
+        for (String date : paymentsSubDocument.keySet()) {
+            paymentsMap.put(LocalDateTime.parse(date), Float.parseFloat(paymentsSubDocument.get(date).toString()));
+        }
+        return paymentsMap;
+    }
+
+    private static Document getDocumentFromMap(Map<LocalDateTime, Float> paymentsMap) {
+        var paymentsDocument = new Document();
+        for (LocalDateTime date : paymentsMap.keySet()) {
+            paymentsDocument.put(date.toString(), paymentsMap.get(date));
+        }
+        return paymentsDocument;
     }
 
 }
